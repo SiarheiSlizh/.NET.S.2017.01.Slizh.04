afe1dd0dd351636443ce0d5169688a6b36e11537
сергей слиж <sergeislizh@mail.ru>
2017-03-20 02:12:42 +0300

    //

---
+++ Task3_4/WorkingWithNumbers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_4
+{
+    /// <summary>
+    /// This class works with numbers.
+    /// </summary>
+    public static class WorkingWithNumbers
+    {
+        #region NextBiggerNumber
+        /// <summary>
+        /// This method finds next bigger number, which consists of this digits.
+        /// </summary>
+        /// <param name="number">Initial number for which is found next bigger number.</param>
+        /// <returns></returns>
+        public static int NextBiggerNumber(int number)
+        {
+            if (number < 0)
+                throw new ArgumentException();
+
+            string num = number.ToString();
+            byte[] arrDigit = new byte[num.Length];
+
+            for (int j = num.Length - 1; j > -1; j--)
+            {
+                arrDigit[j] = (byte)(number % 10);
+                number /= 10;
+            }
+
+            byte[] arrPartDigit = null;
+            byte buf = byte.MinValue;
+
+            int i = num.Length - 1;
+            for (; i > 0; i--)
+            {
+                if (arrDigit[i - 1] < arrDigit[i])
+                {
+                    buf = arrDigit[i - 1];
+                    arrPartDigit = new byte[num.Length - i];
+                    Array.Copy(arrDigit, i, arrPartDigit, 0, arrPartDigit.Length);
+                    Array.Sort(arrPartDigit);
+                    break;
+                }
+            }
+
+            if (i == 0) return -1;
+            else
+            {
+                for (int j = 0; j < arrPartDigit.Length; j++)
+                    if (arrPartDigit[j] > buf)
+                    {
+                        arrDigit[i - 1] = arrPartDigit[j];
+                        arrPartDigit[j] = buf;
+                        Array.Copy(arrPartDigit, 0, arrDigit, i, arrPartDigit.Length);
+                        break;
+                    }
+
+                return int.Parse(string.Concat(arrDigit.Take(num.Length)));
+            }
+        }
+        #endregion
+
+        #region NewtonMethod
+        /// <summary>
+        /// This method calculates the root of n-degree of number.
+        /// </summary>
+        /// <param name="number">Digit for which the method finds the root.</param>
+        /// <param name="n">Degree of number.</param>
+        /// <param name="eps">Predetermained occuracy.</param>
+        /// <returns></returns>
+        public static double NewtonMethod(double number, int n, double eps)
+        {
+            if (n < 0)
+                throw new ArgumentException();
+
+            if ((n % 2 == 0  && number < 0) || eps < 0)
+                throw new ArithmeticException();
+
+            double x = 1;
+            while (true)
+            {
+                double nx = ((n - 1) * x + number / Math.Pow(x, n - 1)) / n;
+                if (Math.Abs(x - nx) < eps) return x;
+                x = nx;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Task3_4Tests/WorkingWithNumbersTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task3_4;
+
+namespace Task3_4Tests
+{
+    [TestFixture]
+    public class WorkingWithNumbersTests
+    {
+        #region NextBiggerNumber
+
+        [TestCase(-4)]
+        public void NextBiggerNumber_ArgumentException(int number)
+        {
+            Assert.Throws<ArgumentException>(() => WorkingWithNumbers.NextBiggerNumber(number));
+        }
+        
+
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]
+        [TestCase(20, ExpectedResult = -1)]
+        public int NextBiggerNumber_PositiveTests(int number)
+        {
+            return WorkingWithNumbers.NextBiggerNumber(number);
+        }
+        #endregion
+        #region NewtonMethod
+
+        [TestCase(3,-2,0.01)]
+        public void NewtonMethod_ArgumentException(double number, int n, double eps)
+        {
+            Assert.Throws<ArgumentException>(() => WorkingWithNumbers.NewtonMethod(number, n, eps));
+        }
+
+
+        [TestCase(4, 2, -0.1)]
+        [TestCase(-4, 2, 0.01)]
+        [TestCase(3, 3, -0.1)]
+        public void NewtonMethod_ArithmeticException(double number, int n, double eps)
+        {
+            Assert.Throws<ArithmeticException>(() => WorkingWithNumbers.NewtonMethod(number, n, eps));
+        }
+
+
+        [TestCase(30.25, 2, 0.0000000001, ExpectedResult = 5.5)]
+        [TestCase(166.375, 3, 0.000000000000001, ExpectedResult = 5.5)]
+        [TestCase(69.934528, 3, 0.0000000000000001, ExpectedResult = 4.12)]
+        public double NewtonMethod_PositiveTests(double number, int n, double eps)
+        {
+            return WorkingWithNumbers.NewtonMethod(number, n, eps);
+        }
+        #endregion
+    }
+}
